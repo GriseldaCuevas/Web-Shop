14a4341bde84608adfb831bfeb2565b1e80a7f1a
Pedro R. Enriquez <pedorenriquez81@gmail.com>
2015-07-08 15:39:20 -0600

    Se agregaron las migraciones que son: Productos, Categorias, Usuarios, Ventas y Productos_Venta

---
+++ ejemplo78/database/migrations/2015_08_07_000002_create_categorias_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('password_resets');
+    }
+}
---
+++ ejemplo78/database/migrations/2015_08_07_000004_create_ventas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('id_usuario')->unsigned();
+            $table->foreign('id_usuario')->references('id')->on('usuarios');
+            $table->double('total', 8, 2);
+            $table->dateTime('fecha');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
